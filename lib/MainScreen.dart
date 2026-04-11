fa310ac2e47380313747b44ba02731ed482caf28
Harsh Patel <patelharsh98000@gmail.com>
2026-04-11 14:09:23 +0530

    (User- App)    1.Forgot password UI issue resolved.    2.Edit type map focus issue resolved..    3.Added eye icon (SVG) in password fields on login and signup screens.    4.Text field border UI issue resolved.    5. Home screen search issue resolved.    6.Added SVG icons in home screen bottom bar.   7.Added Continue button at the bottom in Content Type screen. 8.Updated S3 bucket environment variables and refactored the s3Upload function. 9.Updated names of 3 shoot types.

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -7,7 +7,6 @@ import 'package:flutter_svg/svg.dart';
 import 'Booking/booking_all_screen.dart';
 import 'Home/NewBookingFlow/CreateProjectStep1/Content_Type_screen.dart';
 import 'Home/New_Home/new_home_screen.dart';
-import 'Home/Specialities/specialities.dart';
 import 'Home/home_screen.dart';
 
 class Mainscreen extends StatefulWidget {
@@ -136,7 +135,7 @@ class _MainscreenState extends State<Mainscreen> {
       path,
       height: 28,
       width: 28,
-      fit: BoxFit.contain,
+      fit: BoxFit.none,
     );
   }
 }
