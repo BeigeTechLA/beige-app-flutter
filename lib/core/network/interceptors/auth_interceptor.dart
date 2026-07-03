ee3fd5ce5c5801234e74bb1be602f4ed11adb541
“Krunal <krunal@revurge.com>
2026-07-03 16:45:24 +0530

    fix(network): only trigger 401 handler when request had auth token
    Track whether the outgoing request carried a bearer token via options.extra['_hadAuthToken']. In onError, gate onUnauthorized on that flag so anonymous requests receiving a 401 don't force a logout.

--- lib/core/network/interceptors/auth_interceptor.dart
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -16,6 +16,9 @@ class AuthInterceptor extends QueuedInterceptor {
 
     if (token != null && token.isNotEmpty) {
       options.headers['Authorization'] = 'Bearer $token';
+      options.extra['_hadAuthToken'] = true;
+    } else {
+      options.extra['_hadAuthToken'] = false;
     }
 
     options.headers['Accept'] = 'application/json';
@@ -25,7 +28,10 @@ class AuthInterceptor extends QueuedInterceptor {
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
+    final hadToken = err.requestOptions.extra['_hadAuthToken'] == true;
+
     if (err.response?.statusCode == 401 &&
+        hadToken &&
         onUnauthorized != null &&
         !_handlingUnauthorized) {
       _handlingUnauthorized = true;
